7ed6ecac2c9ad8eba9037e156487b9c088253515
Nick Ly <nickly88@pm.me>
2026-09-07 15:48:18 -0400

    fix

--- so-scheduling/src/lib/mongodb.ts
+++ so-scheduling/src/lib/mongodb.ts
@@ -25,7 +25,7 @@ async function dbConnect() {
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
-      dbName: process.env.NODE_ENV === 'production' ? 'prod' : 'dev',
+      dbName: process.env.NODE_ENV == 'production' ? 'prod' : 'dev',
       maxPoolSize: 10,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
@@ -36,7 +36,7 @@ async function dbConnect() {
       return mongoose;
     });
   }
-  
+
   try {
     cached.conn = await cached.promise;
   } catch (e) {
